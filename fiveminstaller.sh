0b19adec9f0042ffb00546217bfc1608f6af17cd
NotTheCray <132234780+NotTheCray@users.noreply.github.com>
2023-10-05 18:57:50 +0200

    published V2
    I just maded the installer better now and better to handle

--- fiveminstaller.sh
+++ fiveminstaller.sh
@@ -1,36 +1,88 @@
 #!/bin/bash
 
-echo 'Was geht ab Bro, Installer startet Jetzt'
+green='\033[0;32m'
+red='\033[0;31m'
+white='\033[0;37m'
+reset='\033[0;0m'
 
+status(){
+  clear
+  if [[ "$2" == "/" ]]; then
+    echo -e $green$1$reset
+  else
+    echo -e $green$@'...'$reset
+  fi
+  sleep 1
+}
 
-apt update && apt upgrade
+runCommand(){
+    COMMAND=$1
 
+    if [[ ! -z "$2" ]]; then
+      status $2
+    fi
 
-apt install xf tar
+    eval $COMMAND;
+    BASH_CODE=$?
+    if [ $BASH_CODE -ne 0 ]; then
+      echo -e "${red}An error occurred:${reset} ${white}${COMMAND}${reset}${red} returned${reset} ${white}${BASH_CODE}${reset}"
+      exit ${BASH_CODE}
+    fi
+}
 
+function updateUpgrade() {
+    status "System wird aktualisiert"
+    runCommand "sudo apt-get update"
+    runCommand "sudo apt-get upgrade -y"
+}
 
-mkdir -p /home/FiveM/server
-cd /home/FiveM/server
+function installDependencies() {
+    status "Abhängigkeiten werden installiert"
+    runCommand "apt install xf tar"
+    runCommand "apt install screen"
+}
 
+function Direction () {
+    status "Ordner wird erstellt"
+    runCommand "mkdir -p /home/FiveM/server"
+    runCommand "cd /home/FiveM/server"
+}
 
-echo 'Gebe den link zu deinen FiveM-Artifakten ein:'
-read link
-wget $link
+function ArifactsInstall() {
+    status "Gebe den artifacts link ein"
+    read -p "Artifacts Link: " artifacts
+    runCommand "wget $artifacts"
+    sleep 1
 
+    status "Entpacke die artifacts"
+    runCommand "tar xf fx.tar.xz"
+    runCommand "rm fx.tar.xz"
+    runCommand "chmod +x run.sh"
+}
 
-echo 'Nun noch einmal entpacken...'
-tar xf fx.tar.xz
-echo 'Bums sind sie installiert'
+function StartingScreenOptions() {
+    status "Möchtest du den server jetzt direkt starten?"
+    Options "Ja" "Nein"
 
-rm -r fx.tar.xz
+    case $OPTIONS in
+        1) StartingScreen;;
+        2) exit;;
+    esac
+}
 
-echo 'Jetzt noch einmal die Screen anwendung installieren...'
-apt install screen
+function StartingScreen() {
+    status "Danke fürs benutzen des FiveM Installers von Crayy"
+    sleep 2
+    status "Der Server wird gestartet"
+    runCommand "screen -dmS FiveM ./run.sh"
+}
 
-echo 'Alles hat geklappt! Gehe einfach in /home/FiveM/server und benutze den command --> screen ./run.sh'
+curl --version
+if [[ $? == 127  ]]; then  apt -y install curl; fi
 
-cd /home/FiveM/server
+updateUpgrade
+installDependencies
+Direction
+ArifactsInstall
+StartingScreenOptions
 
-screen ./run.sh
-
-echo 'So Fertig Kollege!'
